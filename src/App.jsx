59fc8fa191bec5501b4460c24036acd43e29d564
keshavnagar <keshavnagar887@gmail.com>
2026-02-22 21:56:26 +0530

    create the first card using the api

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const App = () => {
   const [apiData, setApiData] = useState(null);
   const [loading, setLoading] = useState(true)
   const [err, setErr] = useState(null)
-  const API = "https://pokeapi.co/api/v2/pokemons/pikachu";
+  const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
   const fatchPokemon = () => (
     fetch(API)
       .then((res) => res.json())
@@ -19,11 +19,27 @@ const App = () => {
   if (err)
     return <div><h1>{err.message}</h1></div>
   return (
-    <ul>
-      <h1>{apiData.name}</h1>
-      <h2>{apiData.order}</h2>
-      <img src={apiData.sprites.other.dream_world.front_default} alt="pokemon" />
-    </ul>
+    // <ul>
+    //   <h1>{apiData.name}</h1>
+    //   <h2>{apiData.order}</h2>
+    //   <img src={apiData.sprites.other.dream_world.front_default} alt="pokemon" />
+    // </ul>
+    <section className="container">
+      <header>
+        <h1>Let's catch pokemon</h1>
+      </header>
+      <li className="pokemon-card">
+        <figure>
+          <img src={apiData.sprites.other.dream_world.front_default} alt="pokemon" />
+        </figure>
+        <h1>{apiData.name}</h1>
+        <div className="grid-three-cols">
+          <p className="pokemon-info">Height: <span>{apiData.height}</span></p>
+          <p className="pokemon-info">Weight: <span>{apiData.weight}</span></p>
+          <p className="pokemon-info">Speed: <span>{apiData.stats[5].base_stat}</span></p>
+        </div>
+      </li>
+    </section>
   );
 };
 export default App;
