53d6187743ae4439e5406ac265df037bbf46de3a
keshavnagar <keshavnagar887@gmail.com>
2026-02-22 20:50:59 +0530

    complete the concept of err state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react";
 const App = () => {
   const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(true)
+  const [err, setErr] = useState(null)
   const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
   const fatchPokemon = () => (
     fetch(API)
       .then((res) => res.json())
-      .then((data) => setApiData(data))
-      .catch((err) => console.log(err))
+      .then((data) => { setApiData(data); setLoading(false) })
+      .catch((err) => { console.log(err); setErr(err); setLoading(false) })
   )
   useEffect(() => {
     fatchPokemon();
   }, []);
 
-  console.log(apiData)
-
-  if (!apiData)
-    return <h1> Loading......</h1>
+  if (loading)
+    return <div><h1>Loading....</h1></div>
+  if (err)
+    return <div><h1>{err.message}</h1></div>
   return (
     <ul>
       <h1>{apiData.name}</h1>
